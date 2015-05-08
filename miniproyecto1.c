a61efa47553c33b4035f4abab6ec0d5148483e65
richard <richardlm57@gmail.com>
2015-05-08 13:03:14 -0430

    for con mas o menos lo que hay que hacer, hay que hacer fork

--- miniproyecto1.c
+++ miniproyecto1.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 
 int main(){
-	int socketfd, localport, bindsuccess;
-	struct sockaddr_in socketbind;
+	int socketfd, localport, acceptsock;
+	struct sockaddr_in socketbind, clientaddr;
 
 	localport=32000;
 	struct hostent* localhost = gethostbyname( "localhost" );
@@ -31,9 +36,45 @@ int main(){
 	socketbind.sin_port = htons(localport);
 
 	printf("Haciendo bind\n");
-	if((bindsuccess=bind(socketfd,(struct sockaddr*)&socketbind,sizeof(socketbind))) < 0) {
+	if(bind(socketfd,(struct sockaddr*)&socketbind,sizeof(socketbind)) < 0) {
 		printf("Error en bind() \n");
 		exit(-1);
 	}
-	printf("Bind del carajo: %d\n",bindsuccess);
+
+	printf("Escuchando\n");
+	if((listen(socketfd,5) != 0){
+		printf("Error en listen() \n");
+		exit(-1);
+	}
+
+	for (;;){
+		/* Accept actual connection from the client */
+		acceptsock = accept(socketfd, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
+		if (acceptsock < 0){
+			perror("Error en accept");
+			exit(-1);
+		}
+
+		/* If connection is established then start communicating */
+		bzero(buffer,256);
+		n = read( acceptsock,buffer,255 );
+
+		if (n < 0)
+		{
+			perror("ERROR reading from socket");
+			exit(1);
+		}
+
+		printf("Here is the message: %s\n",buffer);
+
+		/* Write a response to the client */
+		n = write(acceptsock,"I got your message",18);
+
+		if (n < 0)
+		{
+			perror("ERROR writing to socket");
+			exit(1);
+		}
+	}	  
+	exit(0);
 }
