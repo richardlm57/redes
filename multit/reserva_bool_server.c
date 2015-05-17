97c42c58175255aad92bc519bd81a63c9715c7d9
“Patricia“ <patricia.reinoso12@gmail.com>
2015-05-16 22:33:38 -0430

    listo los mensajes

--- multit/reserva_bool_server.c
+++ multit/reserva_bool_server.c
@@ -22,25 +22,6 @@ bool_t
 make_reservation_3_svc(seat *myseat, int *result, struct svc_req *rqstp)
 {
 	bool_t retval;
-
-	if (train[myseat->row - 1][myseat->column -1] == 0){
-		train[myseat->row - 1][myseat->column - 1] = 1;
-		retval = 1;
-		*result = 1; 
-	}
-	else{
-		retval = 1;
-		*result = 0;
-	}
-	
-	//printf("Clientadress:%s\n", inet_ntoa(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr));
-	FILE *f = fopen("log-server", "a");
-	if (f == NULL)
-	{
-	    printf("Error opening file!\n");
-	    exit(1);
-	}	
-
 	time_t rawtime;
 	struct tm *info;
 	char buffer[80];
@@ -52,10 +33,33 @@ make_reservation_3_svc(seat *myseat, int *result, struct svc_req *rqstp)
 	strcpy(buffer,asctime(info));
 	token = strtok(buffer, "\n");
 
-   	fprintf(f,"[%s] [error] [cliente %hu.%hu.%hu.%hu]: Reservación del puesto: Fila %d, Columna %d.\n", 
-   	token, SPLIT_S_ADDR_INTO_BYTES(ntohl(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr)),
-   	myseat->row, myseat->column);
+	FILE *f = fopen("log-server", "a");
+	if (f == NULL){
 
+	    printf("Error al abrir el archivo\n");
+	    retval = 0;
+	}
+	
+	if (train[myseat->row - 1][myseat->column -1] == 0){
+		train[myseat->row - 1][myseat->column - 1] = 1;
+		
+		fprintf(f,"[%s] [error] [cliente %hu.%hu.%hu.%hu]: Puesto: Fila %d, Columna %d reservado.\n", 
+	   	token, SPLIT_S_ADDR_INTO_BYTES(ntohl(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr)),
+	   	myseat->row, myseat->column);
+
+		retval = 1;
+		*result = 1; 
+	}
+	else{
+
+		fprintf(f,"[%s] [error] [cliente %hu.%hu.%hu.%hu]: Puesto: Fila %d, Columna %d ocupado.\n", 
+	   	token, SPLIT_S_ADDR_INTO_BYTES(ntohl(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr)),
+	   	myseat->row, myseat->column);
+
+		retval = 1;
+		*result = 0;
+	}
+	
 	fclose(f);
 	return retval;
 }
@@ -87,6 +91,24 @@ seats_3_svc(void *argp, char **result, struct svc_req *rqstp)
 	char temp[4];
 	strcpy(buffer,"");
 
+	time_t rawtime;
+	struct tm *info;
+	char buffer2[80];
+	char *token;
+
+	time( &rawtime );
+
+	info = localtime( &rawtime );
+	strcpy(buffer2,asctime(info));
+	token = strtok(buffer2, "\n");
+
+	FILE *f = fopen("log-server", "a");
+	if (f == NULL){
+
+	    printf("Error al abrir el archivo\n");
+	    retval = 0;
+	}
+
 	memset(*result,' ',170);
 
 	for (i = 0; i < 10; i++){
@@ -100,6 +122,17 @@ seats_3_svc(void *argp, char **result, struct svc_req *rqstp)
 	}
 	strcpy(*result,buffer);
 	retval = 1;
+
+	if (strcmp(*result,"") == 0){
+		fprintf(f,"[%s] [error] [cliente %hu.%hu.%hu.%hu]: Vagón completo.\n", 
+		   	token, SPLIT_S_ADDR_INTO_BYTES(ntohl(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr)));
+	}
+	else{
+		fprintf(f,"[%s] [error] [cliente %hu.%hu.%hu.%hu]: Lista de puestos disponibles.\n", 
+		   	token, SPLIT_S_ADDR_INTO_BYTES(ntohl(rqstp->rq_xprt->xp_raddr.sin_addr.s_addr)));
+
+	}
+	fclose(f);
 	return 1;
 }
 
